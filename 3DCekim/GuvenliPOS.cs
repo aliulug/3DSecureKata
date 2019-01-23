089442d9d8e97d935855f19ab016757c249f480c
Ali Uluğ <keremaliulug@gmail.com>
2019-01-23 20:45:27 +0300

    Init

---
+++ 3DCekim/GuvenliPOS.cs
@@ -0,0 +1,59 @@
+﻿using AdaGenel.Cesitli;
+using AdaPublicGenel.Extensions;
+using AdaPublicGenel.Genel;
+
+namespace _3DCekim
+{
+	public class GuvenliPOS
+	{
+		private readonly IBankaSecen _bankaSecen;
+		private readonly IGuvenliPOSVeritabaniVekili _veritabaniVekili;
+
+		public GuvenliPOS(IBankaSecen bankaSecen, IGuvenliPOSVeritabaniVekili veritabaniVekili)
+		{
+			_bankaSecen = bankaSecen;
+			_veritabaniVekili = veritabaniVekili;
+		}
+
+		public IstekSonuc<BankaCekimIstegiSonucu> CekimIstegiGonder(SiparisBilgi siparisBilgi)
+		{
+			IstekSonuc siparisBilgiOnKontrolSonucu = siparisBilgileriGecerliMi(siparisBilgi);
+			if (siparisBilgiOnKontrolSonucu.Basarili.Degil())
+				return siparisBilgiOnKontrolSonucu;
+
+			IBanka banka = _bankaSecen.BankaSec(siparisBilgi);
+
+			//string guid = bankayaGonderilecekGuidOlustur();
+			string guid = "";
+
+			var cekimIstegiSonuc = banka?.CekimIstegiGonder(siparisBilgi);
+
+			_veritabaniVekili.Bankadan3DOnayiBekleyenIslemKaydet(siparisBilgi, guid, cekimIstegiSonuc.Nesne);
+
+			return cekimIstegiSonuc;
+		}
+
+		private string bankayaGonderilecekGuidOlustur()
+		{
+			return Araclar.RastgeleString(10);
+		}
+
+		private IstekSonuc siparisBilgileriGecerliMi(SiparisBilgi siparisBilgi)
+		{
+			if (siparisBilgi.Kart == null)
+				return IstekSonuc.Hata("Sipariş bilgisinde kart bilgileri bulunmalı");
+
+			if (siparisBilgi.Kart.KartNo.Length != 15 && siparisBilgi.Kart.KartNo.Length != 16)
+				return IstekSonuc.Hata("Kredi kartı numarası 15 veya 16 hane olmalı");
+
+			if (siparisBilgi.Kart.GuvenlikKodu.Length != 3)
+				return IstekSonuc.Hata("Kart güvenlik numarası 3 hane olmalı");
+
+
+			//todo............
+
+			return IstekSonuc.Basari();
+
+		}
+	}
+}
---
+++ 3DCekim/GuvenliPOSTest.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdaPublicGenel.Genel;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace _3DCekim
+{
+	public class GuvenliPOSTest
+	{
+		private IBankaSecen _bankaSecen;
+		private GuvenliPOS _pos;
+		private SiparisBilgi _siparisBilgi;
+		private IBanka _banka;
+		private string _banka3DOnayUrl;
+		private BankaCekimIstegiSonucu _bankaSonuc;
+		private IstekSonuc<BankaCekimIstegiSonucu> _bankaninCevabi;
+		private IGuvenliPOSVeritabaniVekili _veritabaniVekili;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_bankaSecen = Substitute.For<IBankaSecen>();
+			_veritabaniVekili = Substitute.For<IGuvenliPOSVeritabaniVekili>();
+
+			_pos = new GuvenliPOS(_bankaSecen, _veritabaniVekili);
+			_siparisBilgi = new SiparisBilgi
+			{
+				Kart = new KrediKarti
+				{
+					KartNo = "0123456789123456",
+					AdSoyad = "Ali Uluğ",
+					GuvenlikKodu = "123",
+					SonKullanmaYil = 2020,
+					SonKullanmayAy = 1
+				},
+				Tutar = 100,
+				TaksitAdedi = 2
+			};
+			_banka = Substitute.For<IBanka>();
+			_bankaSecen.BankaSec(Arg.Any<SiparisBilgi>()).Returns(_banka);
+
+
+			_banka3DOnayUrl = "https://garanti.com";
+			_bankaSonuc = new BankaCekimIstegiSonucu
+			{
+				BankaSistemi3DOnayUrl = _banka3DOnayUrl
+			};
+			_bankaninCevabi = IstekSonuc<BankaCekimIstegiSonucu>.Basari("", _bankaSonuc);
+			_banka.CekimIstegiGonder(_siparisBilgi).Returns(_bankaninCevabi);
+
+		}
+
+		[Test]
+		public void CekiminHangiBankadanYapilacaginaKararVerilir()
+		{
+			//when
+			_pos.CekimIstegiGonder(_siparisBilgi);
+
+			//then
+			_bankaSecen.Received().BankaSec(_siparisBilgi);
+		}
+
+		[Test]
+		public void BankayaIstekGonderilir()
+		{
+			//given
+
+			//when
+			_pos.CekimIstegiGonder(_siparisBilgi);
+
+			//then
+			_banka.Received().CekimIstegiGonder(_siparisBilgi);
+		}
+
+		[Test]
+		public void SiparisBilgisindeKartNumarasiEksikseHataDonerBankaSecilmez()
+		{
+			SiparisBilgi gecersizBilgi = new SiparisBilgi
+			{
+				Kart = new KrediKarti
+				{
+					KartNo = "",
+					AdSoyad = "Ali Uluğ",
+					GuvenlikKodu = "123",
+					SonKullanmaYil = 2020,
+					SonKullanmayAy = 1
+				},
+				Tutar = 100,
+				TaksitAdedi = 2
+			};
+
+			IstekSonuc<BankaCekimIstegiSonucu> sonuc = _pos.CekimIstegiGonder(gecersizBilgi);
+
+			Assert.IsFalse(sonuc.Basarili);
+			_bankaSecen.DidNotReceive().BankaSec(Arg.Any<SiparisBilgi>());
+		}
+
+		[Test]
+		public void BankadanDonen3DOnayUrlIstemciyeDoner()
+		{
+
+			IstekSonuc<BankaCekimIstegiSonucu> sonuc = _pos.CekimIstegiGonder(_siparisBilgi);
+
+			Assert.AreEqual(sonuc.Nesne.BankaSistemi3DOnayUrl, _banka3DOnayUrl);
+		}
+
+		[Test]
+		public void BankayaCekimIstegiGonderildigindeAsenkronCevaptaKullanilmakUzereSiparisBilgisiKaydedilir()
+		{
+			_pos.CekimIstegiGonder(_siparisBilgi);
+
+			_veritabaniVekili.ReceivedWithAnyArgs().Bankadan3DOnayiBekleyenIslemKaydet(_siparisBilgi, "", _bankaSonuc);
+		}
+	}
+
+	public interface IGuvenliPOSVeritabaniVekili
+	{
+		void Bankadan3DOnayiBekleyenIslemKaydet(SiparisBilgi siparisBilgi, string guid, BankaCekimIstegiSonucu bankaSonuc);
+	}
+
+	public interface IBankaSecen
+	{
+		IBanka BankaSec(SiparisBilgi siparisBilgi);
+	}
+
+	public interface IBanka
+	{
+		IstekSonuc<BankaCekimIstegiSonucu> CekimIstegiGonder(SiparisBilgi siparisBilgi);
+	}
+
+	public class BankaCekimIstegiSonucu
+	{
+		public string BankaSistemi3DOnayUrl;
+	}
+
+	public class SiparisBilgi
+	{
+		public KrediKarti Kart;
+		public double Tutar;
+		public int TaksitAdedi;
+
+		public string SiparisAnahtar;//poliçe zeyil key olabilir veya kullanım amacına göre değişir
+		public string PassUrl;
+		public string FailUrl;
+
+
+	}
+
+	public class KrediKarti
+	{
+		public string KartNo;
+		public string AdSoyad;
+		public string GuvenlikKodu;
+		public int SonKullanmayAy;
+		public int SonKullanmaYil;
+
+	}
+}
